a926966af3e47857c5d5aa692ff149b18ce47a15
Rakibul-Hasan270 <rakibx270@gmail.com>
2024-10-27 18:16:22 +0600

    set display element,bg color,update score and update life

--- scripts/alphaPro.js
+++ scripts/alphaPro.js
@@ -1,3 +1,33 @@
+function keyupFunction(even) {
+    const gamerPress = even.key;
+
+    const currentAlphabetElement = document.getElementById('show-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const alphabet = currentAlphabet.toLowerCase();
+
+    if (alphabet === gamerPress) {
+        const currentScore = getElementByIdParseint('current-score');
+        const newScore = currentScore + 1;
+        setTextElementById('current-score', newScore);
+
+        continueGame();
+        removeBgColor(alphabet);
+    }
+    else {
+        const currentLife = getElementByIdParseint('current-life');
+        const restLife = currentLife - 1;
+        setTextElementById('current-life', restLife);
+
+        if(restLife===0){
+            
+        }
+    }
+
+    // console.log(gamerPress, alphabet)
+}
+
+document.addEventListener('keyup', keyupFunction)
+
 function continueGame() {
     const alphabet = randomAlphabet();
     setBgColor(alphabet);
@@ -10,5 +40,4 @@ function play() {
     addClassList('home');
     removeClassList('play-ground');
     continueGame();
-    bgColor()
 }
--- scripts/ulity.js
+++ scripts/ulity.js
@@ -13,6 +13,24 @@ function setBgColor(elementId) {
     element.classList.add('bg-orange-400');
 }
 
+function removeBgColor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+// if else block-----
+function setTextElementById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementByIdParseint(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const elementValue = parseInt(elementText);
+    return elementValue;
+}
+
 function randomAlphabet() {
     const element = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = element.split('');
